3898d7641d218731318aaedb66ce80006ba63594
josephjohn2806 <josephsjohn2806>
2026-03-08 17:16:14 +0300

    add exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,80 @@
 using System;
-
-class Program
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        static void Main(string[] args)
+        {
+            int percent;
+            while (true)
+            {
+                Console.Write("What is your grade percentage? ");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out percent))
+                {
+                    break;
+                }
+                Console.WriteLine("Please enter a valid number.");
+            }
+
+            string letter = "";
+
+            if (percent >= 90)
+            {
+                letter = "A";
+            }
+            else if (percent >= 80)
+            {
+                letter = "B";
+            }
+            else if (percent >= 70)
+            {
+                letter = "C";
+            }
+            else if (percent >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+            string sign = "";
+            int lastDigit = percent % 10;
+
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+
+            if (letter == "A" && sign == "+")
+            {
+                sign = "";
+            }
+            
+            if (letter == "F")
+            {
+                sign = "";
+            }
+
+            Console.WriteLine($"Your grade is: {letter}{sign}");
+
+            if (percent >= 70)
+            {
+                Console.WriteLine("Congratulations! You passed the course.");
+            }
+            else
+            {
+                Console.WriteLine("Don't give up! You'll get it next time.");
+            }
+        }
     }
 }
