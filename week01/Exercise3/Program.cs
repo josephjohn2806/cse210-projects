cd06a5fa6a039be018d2b59e81973ef3f7c90962
josephjohn2806 <josephsjohn2806>
2026-03-08 17:12:58 +0300

    add exercise 1-5

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,67 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-class Program
+namespace Exercise4
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int userNumber = -1;
+
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+            while (userNumber != 0)
+            {
+                Console.Write("Enter number: ");
+                userNumber = int.Parse(Console.ReadLine());
+
+                if (userNumber != 0)
+                {
+                    numbers.Add(userNumber);
+                }
+            }
+
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            double average = (double)sum / numbers.Count;
+
+            int max = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {max}");
+
+            // Stretch Challenges
+            int smallestPositive = int.MaxValue;
+            foreach (int number in numbers)
+            {
+                if (number > 0 && number < smallestPositive)
+                {
+                    smallestPositive = number;
+                }
+            }
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+
+            numbers.Sort();
+            Console.WriteLine("The sorted list is:");
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
     }
 }
