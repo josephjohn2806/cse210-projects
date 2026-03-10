a5cfb3af7d67d61d61aa9f0ea817632f41fb941e
josephjohn2806 <josephsjohn2806>
2026-03-10 12:49:48 +0300

    add all of the changes

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine("Your name is " + lastName + ", " + firstName + ", " + lastName + ".");
+    }
+}
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101); // number between 1 and 100
+        int guess = -1;
+
+        Console.WriteLine("Welcome to the Guess My Number Game!");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+    }
+}
